7a860611306eac5b07432b089dee5bcd2621d5f3
Suvigya Tripathi <suvigya08@gmail.com>
2022-05-09 03:54:36 +0530

    third commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,4 @@
-// import React from 'react'
-// // create boiler plate using rfc and enter
-// export default function footer() {
-//   // Add styles like this
-//   let footerStyle = {
-//     position: "relative",
-//     top: "10vh",
-//     width: "100%",
-//   }
-//   return (
-//     <footer className="bg-dark text-light py-3"  style={footerStyle}>
-//       <p className="text-center">
-//         Copyright &copy; Suvigya Tripathi
-// </p>
-//     </footer>
-//   )
-// }
+
 import React from 'react'
 
 function Footer() {
